e0941a01238768fdc6eb19abfa476f3920981c54
Matěj Štágl <stagl.matej@centrum.cz>
2025-01-05 07:06:11 +0100

    comp-time top level entropy for the native target

---
+++ TeReoLocalizer/TeReoLocalizer/build/GenerateHostPage.cs
@@ -0,0 +1,46 @@
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+
+namespace TeReoLocalizer.build;
+
+public class GenerateHostPage : Microsoft.Build.Utilities.Task
+{
+    [Required]
+    public string OutputPath { get; set; }
+    [Required]
+    public string ProjectDir { get; set; }
+    [Required]
+    public string Entropy { get; set; }
+
+    public override bool Execute()
+    {
+        string content = GenerateHtmlContent();
+        File.WriteAllText(OutputPath, content);
+        return true;
+    }
+    
+    private string GenerateHtmlContent()
+    {
+        string templatePath = Path.Combine(ProjectDir, "wwwroot", "index.html");
+        
+        if (File.Exists(templatePath))
+        {
+            string template = File.ReadAllText(templatePath);
+            
+            template = template.Replace("{ENTROPY}", $"_{Entropy}");
+            template = $"<!-- this is a generated file, do not modify manually. Changes will be lost.--!>\n{template}";
+            
+            return template;
+        }
+        
+        return $"""
+                <!DOCTYPE html>
+                <html>
+                <body>
+                <p>Missing file {templatePath}</p>
+                </body>
+                </html>
+                """;
+    }
+}
